f5936e8c84588a05464d135e8df9ccaf547a9ba8
JiyeHe8462 <98429454+JiyeHe8462@users.noreply.github.com>
2022-10-04 21:10:37 -0400

    Update apply_imnormalize.m

--- apply_imnormalize.m
+++ apply_imnormalize.m
@@ -1,9 +1,9 @@
 function [img_norm] = apply_imnormalize(image_n)
     %get size infor. of input image
-    [row, column] = size(image_n);
-    for channel = 1:3
-       for row = row
-           for column = column
+    [row, column, channel] = size(image_n);
+    for channel = 1:channel
+       for row = 1:row
+           for column = 1:column
                %performing normalization
                img_norm(row,column,channel) = image_n(row,column,channel)/(255-0.5);
            end
