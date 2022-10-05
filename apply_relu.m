94f3ac7176e010dfd032f00e95b15a7dd4e5e9ac
JiyeHe8462 <98429454+JiyeHe8462@users.noreply.github.com>
2022-10-04 21:11:13 -0400

    Update apply_relu.m

--- apply_relu.m
+++ apply_relu.m
@@ -1,9 +1,9 @@
 function [img_relu] = apply_relu(image_r)
     %get row and column infor.
-   [row, column] = size(image_r);
-   for channel = 1:10
-       for row = row
-           for column = column
+   [row, column, channel] = size(image_r);
+   for channel = 1:channel
+       for row = 1:row
+           for column = 1:column
                %compare each pixel with 0; take out negative number
                img_relu(row,column,channel) = max(image_r(row,column,channel),0);
            end
